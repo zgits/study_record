f4c579d0fd651f416830d8c70ac15b547d43c6b5
zgits <2276479295@qq.com>
2020-12-06 22:47:19 +0800

    完善部分spring security

--- spring/src/main/java/com/study/spring/configuration/security/WebSecurityConfig.java
+++ spring/src/main/java/com/study/spring/configuration/security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
@@ -16,7 +17,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.
+                inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+        .and().passwordEncoder(new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence charSequence) {
+                return charSequence.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence charSequence, String s) {
+                return s.equals(charSequence);
+            }
+        });
     }
 
     @Override
@@ -26,6 +40,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+
+        http.
+                authorizeRequests()
+                .antMatchers("/", "/home")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+
     }
+
 }
