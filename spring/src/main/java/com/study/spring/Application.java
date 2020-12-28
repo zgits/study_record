cb66e13678f260ff90bb8211887b5cd73741df81
zgits <2276479295@qq.com>
2020-12-28 23:25:12 +0800

    自定义banner

--- spring/src/main/java/com/study/spring/Application.java
+++ spring/src/main/java/com/study/spring/Application.java
@@ -1,17 +1,38 @@
 package com.study.spring;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.ResourceBanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationBeanNameGenerator;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
+
+import java.io.PrintStream;
+
+
+/**
+ *
+ * @see com.study.spring.Application
+ */
 
 @SpringBootApplication
 @ComponentScan(nameGenerator = AnnotationBeanNameGenerator.class)
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+
+
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.setBanner(new ResourceBanner(new ClassPathResource("banner.txt")));
+
+        springApplication.setBannerMode(Banner.Mode.CONSOLE);
+        springApplication.run(args);
+//
+//        SpringApplication.run(Application.class, args);
     }
 
 }
